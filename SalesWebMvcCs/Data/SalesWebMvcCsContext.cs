4e265d6683f59708eba7a8ff0447e037ef073de3
Joao <juaumjg@hotmail.com>
2023-09-15 16:15:21 -0300

    Adding OtherEntities and second migration

--- SalesWebMvcCs/Data/SalesWebMvcCsContext.cs
+++ SalesWebMvcCs/Data/SalesWebMvcCsContext.cs
@@ -14,6 +14,8 @@ namespace SalesWebMvcCs.Data
         {
         }
 
-        public DbSet<SalesWebMvcCs.Models.Department> Department { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecords { get; set; }
     }
 }
--- SalesWebMvcCs/Migrations/20230915190726_OtherEntities.Designer.cs
+++ SalesWebMvcCs/Migrations/20230915190726_OtherEntities.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SalesWebMvcCs.Data;
 
 namespace SalesWebMvcCs.Migrations
 {
     [DbContext(typeof(SalesWebMvcCsContext))]
-    partial class SalesWebMvcCsContextModelSnapshot : ModelSnapshot
+    [Migration("20230915190726_OtherEntities")]
+    partial class OtherEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
